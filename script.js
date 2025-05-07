04a930ad618bccbd29846290c3310ba351cce2e3
Luis <luisaravena.71@gmail.com>
2025-05-07 08:22:51 -0400

    Fecha

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ document.getElementById("studentForm").addEventListener("submit",function(e){
     e.preventDefault();
     const name=document.getElementById("name").value.trim();
     const lastName=document.getElementById("lastName").value.trim();
+    const fecha=document.getElementById("fecha").value.trim();
     const grade=parseFloat(document.getElementById("grade").value);
 
-    if(!name || !lastName || isNaN(grade) || grade<1 || grade>7){
+    if(!name || !lastName || !fecha || isNaN(grade) || grade<1 || grade>7){
         alert("Error al ingresar Datos")
          return
     }
-    const student={name,lastName,grade}
+    const student={name,lastName,grade,fecha}
 
     students.push(student);
     
@@ -26,6 +27,7 @@ document.getElementById("studentForm").addEventListener("submit",function(e){
             row.innerHTML=`
                 <td>${student.name}</td>
                 <td>${student.lastName}</td>
+                <td>${student.fecha}</td>
                 <td>${student.grade}</td>`;
             tableBody.appendChild(row);
         }
